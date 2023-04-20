101a88b34fc0557e15836cd9352830f7228f8823
DaviLeoMeda <davileo.meda@gmail.com>
2023-04-20 18:08:25 +0200

    Pause (bootstrap not working)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,48 @@
-import { createApp } from 'vue'
+const { createApp } = Vue
 
 createApp({
+
     data() {
         return {
-            count: 0
+            inputText: '',
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+
+            ],
+            error: false
+
+        }
+    },
+    methods: {
+        submitimput() {
+
+            if (inputText != '') {
+                this.todos.unshift({
+                    text: this.inputText,
+                    done: false
+                })
+                inputText = ''
+            } else {
+                this.error = true
+            }
+
+
         }
     }
+
 }).mount('#app')
+
+
+
+
